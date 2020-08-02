665612c81d0b885f6fbd07ac44afc0a445f287bf
lens <lens.huygh@gmail.com>
2020-08-02 23:45:55 +0200

    add bootstrap commandLineRunner

---
+++ src/main/java/be/lens/udemy/guru/reactive/netfluxexample/bootstrap/BootstrapCLR.java
@@ -0,0 +1,32 @@
+package be.lens.udemy.guru.reactive.netfluxexample.bootstrap;
+
+import be.lens.udemy.guru.reactive.netfluxexample.domain.Movie;
+import be.lens.udemy.guru.reactive.netfluxexample.repositories.MovieRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
+
+@Component
+public class BootstrapCLR implements CommandLineRunner {
+    private final MovieRepository movieRepository;
+
+    public BootstrapCLR(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // reset
+        movieRepository.deleteAll().block();
+
+        Flux.just("Silence of the Lambdas", "AEon Flux", "Enter the Mono<void>", "The Fluxxinator",
+                "Back to the Future", "Meet the Fluxes", "Lord of the Fluxes")
+                .map(title -> new Movie(title, UUID.randomUUID().toString()))
+                .flatMap(movieRepository::save)
+                .subscribe(null, null, () -> {
+                    movieRepository.findAll().subscribe(System.out::println);
+                });
+    }
+}
